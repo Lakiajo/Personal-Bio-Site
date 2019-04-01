4f796c45f0df665dc1dd7f534845f6f100f7550c
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-31 21:52:50 -0500

    Final

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Personal Bio Site
+
+This is my personal Bio Site. There are things about me on this page. 
+
+## Getting Started
+```
+$ git clone git@github.com:Lakiajo/Personal-Bio-Site.git
+```
+
+### Prerequisites
+```
+$ npm install -g http-server
+```
+## Image
+Inline-style: 
+![alt text](https://github.com/Lakiajo/Personal-Bio-Site/blob/master/Screen%20Shot%202019-03-31%20at%209.43.50%20PM.png "Screen Grab")
+
+## Running
+Browse to personal directory
+```
+$ hs -p 5000
+```
+in your web browser navigate to 'localhost:5000'
+
+## Author
+- Lakia Jones
--- project.js
+++ project.js
@@ -1,31 +1,31 @@
 console.log('Lakia Jones');
 const projects = [
     {
-        title: "Lakia's Cake of Cups", 
-        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        title: 'Lakias Cake of Cups', 
+        screenshot: 'https://github.com/Lakiajo/Product-Cards/blob/master/Images/Screen%20Shot%202019-03-16%20at%2011.38.26%20AM.png', 
         description: "Product Cards", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
         available: true,
-        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+        url: 'https://github.com/Lakiajo/Product-Cards', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: 'https://github.com/Lakiajo/Product-Cards',
       },
       {
-        title: "Dummy1", 
-        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        title: 'Lakias Cake of Cups', 
+        screenshot: 'https://github.com/Lakiajo/Product-Cards/blob/master/Images/Screen%20Shot%202019-03-16%20at%2011.38.26%20AM.png', 
+        description: "Product Cards", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
         available: true,
-        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+        url: 'https://github.com/Lakiajo/Product-Cards', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: 'https://github.com/Lakiajo/Product-Cards',
       },
       {
-        title: "Dummy2", 
-        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        title: 'Lakias Cake of Cups', 
+        screenshot: 'https://github.com/Lakiajo/Product-Cards/blob/master/Images/Screen%20Shot%202019-03-16%20at%2011.38.26%20AM.png', 
+        description: "Product Cards", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
         available: true,
-        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+        url: 'https://github.com/Lakiajo/Product-Cards', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: 'https://github.com/Lakiajo/Product-Cards',
       }
 ]
 
@@ -39,11 +39,10 @@ const createProjectCards = () => {
     for(let l = 0; l < projects.length; l++){
         domstring += `<div class= "projectsPage">`
         domstring += `<h3>${projects[l].title}</h3>`;
-        domstring += `<p>${projects[l].screenshot}</p>`;
+        domstring += `<img src=${projects[l].screenshot} class="img"></img>`;
         domstring += `<p>${projects[l].description}</p>`;
         domstring += `<p>${projects[l].technologiesUsed}</p>`;
         domstring += `<p>${projects[l].available}</p>`;
-        domstring += `<p>${projects[l].url}</p>`;
         domstring += `<p>${projects[l].githubUrl}</p>`;
         domstring += `</div>`
     }
