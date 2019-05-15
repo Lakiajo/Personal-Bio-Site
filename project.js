be4d2eb372778a60dba8194092b22f22251b8690
Lakia Jones <rachandra_jones@yahoo.com>
2019-05-15 17:11:14 -0500

    Still working

--- project.js
+++ project.js
@@ -27,7 +27,7 @@ const projects = [
         url: 'https://github.com/Lakiajo/Product-Cards', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: 'https://github.com/Lakiajo/Product-Cards',
       }
-]
+];
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
@@ -37,7 +37,7 @@ const printToDom = (divId, textToPrint) => {
 const createProjectCards = () => {
     let domstring = '';
     for(let l = 0; l < projects.length; l++){
-        domstring += `<div class= "projectsPage">`
+        domstring += `<div class= "projectsPage col-4">`
         domstring += `<h3>${projects[l].title}</h3>`;
         domstring += `<img src=${projects[l].screenshot} class="img"></img>`;
         domstring += `<p>${projects[l].description}</p>`;
@@ -49,9 +49,53 @@ const createProjectCards = () => {
     printToDom('projectsPage', domstring);
     };
     
-    
-    const init = () => {
-    createProjectCards();
-    };
-    
-    init();
+  const hideBio = () => {
+      document.getElementById('bioPage').style.display = 'none';
+      };
+  
+  const hideTech = () => {
+      document.getElementById('technologiesPage').style.display = 'none';
+      };
+  
+  const hideProject = () => {
+      document.getElementById('projectsPage').style.display = 'none';
+      };   
+  
+  const showBio = (e) => {
+      if (bioPage.style.display === "none") {
+          bioPage.style.display = "block";
+        } else {
+          bioPage.style.display = "none";
+        }
+   };
+  
+  const showTech = (e) => {
+      if (technologiesPage.style.display === "none") {
+          technologiesPage.style.display = "block";
+        } else {
+          technologiesPage.style.display = "none";
+        }
+  };
+  
+  const showProject = (e) => {
+      if (projectsPage.style.display === "none") {
+        projectsPage.style.display = "block";
+        } else {
+          projectsPage.style.display = "none";
+        }
+  };
+  
+  const eventListener = () => {
+      document.getElementById('navToBio').addEventListener('click',showBio);
+      document.getElementById('navToTechnologies').addEventListener('click',showTech);
+      document.getElementById('navToProjects').addEventListener('click',showProject);
+  };
+  
+  const init = () => {
+      hideBio();
+      hideProject();
+      eventListener();
+      createProjectCards();
+  };
+
+  init();
